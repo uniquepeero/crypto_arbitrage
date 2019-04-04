53b3e08c4264fba5f12eae0416a563154c3a187a
uniquepeero <48791104+uniquepeero@users.noreply.github.com>
2019-04-04 16:33:56 +0500

    Update hitbtc.py

--- hitbtc.py
+++ hitbtc.py
@@ -1,12 +1,25 @@
 import requests
 import json
+import Exchange
 
-class HitBTC:
-    def get_ticker_symbol(self, symbol):
-        url = 'https://api.hitbtc.com/api/2/public/ticker' + symbol
-        response = requests.get(url)
-        return response
+# API URL https://api.hitbtc.com 
+class HitBTC(Exchange):
+	def __init__(self, url, public_key, secret):
+        self.url = url + "/api/2"
+        self.session = requests.session()
+        self.session.auth = (public_key, secret)
 
+	def get_ticker(self, symbol):
+        return self.session.get('%s/public/ticker/%s' % (self.url, symbol)).json()
 
-hitbtc = HitBTC()
-print(hitbtc.get_ticker_symbol('ETHBTC'))
+    def get_size(self, symbol):
+    	session = requests.get('%s/public/orderbook/%s' % (self.url, symbol)).json()
+    	data = {
+    		'ask' : session['ask'][0],
+    		'bid' : session['bid'][0]
+    	}
+    	return self.data
+
+    def trading_commission(self, symbol):
+    	session = requests.get('%s/trading/fee/%s' % (self.url, symbol)).json()
+
